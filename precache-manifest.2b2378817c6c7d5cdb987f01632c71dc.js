78246784e81463f0a15c8f652ea6633cb93dea52
LeungJZ <375082677@qq.com>
2018-12-12 09:43:54 +0800

    部署时间：2018-12-12 09:43:53

---
+++ precache-manifest.2b2378817c6c7d5cdb987f01632c71dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12516f94095daa9958b6",
+    "url": "/react-mobx-cnode/static/js/runtime~main.12516f94.js"
+  },
+  {
+    "revision": "2d39ef01c0360fadb046",
+    "url": "/react-mobx-cnode/static/js/main.2d39ef01.chunk.js"
+  },
+  {
+    "revision": "4bc1fc99c09d30765d8b",
+    "url": "/react-mobx-cnode/static/js/1.4bc1fc99.chunk.js"
+  },
+  {
+    "revision": "2d39ef01c0360fadb046",
+    "url": "/react-mobx-cnode/static/css/main.614f02d8.chunk.css"
+  },
+  {
+    "revision": "4bc1fc99c09d30765d8b",
+    "url": "/react-mobx-cnode/static/css/1.1a451622.chunk.css"
+  },
+  {
+    "revision": "f1a2b5e58209dd787ba119293953d191",
+    "url": "/react-mobx-cnode/index.html"
+  }
+];
